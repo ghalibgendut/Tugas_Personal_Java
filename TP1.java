d4c74e5de3db71124ebd15dcc5b351a2047fd5b4
ghalibgendut <ghalibsasmito@gmail.com>
2022-12-02 18:57:07 +0700

    change potition of function

--- TP1.java
+++ TP1.java
@@ -7,7 +7,49 @@ import java.util.Scanner;
 class TP1 {
 
     public static void main(String[] args) {
-        ramalan();
+        gudang();
+    }
+
+    public static void gudang() {
+        Scanner input = new Scanner(System.in);
+
+        String name, productName;
+        double buyPrice, sellPrice;
+        int productNumber;
+
+        System.out.print("Masukan nama anda : ");
+        name = input.nextLine();
+
+        System.out.print("\n");
+        System.out.println("Stok Gudang Input Barang");
+        System.out.println("##########################\n\n");
+
+        System.out.println("Selamat Datang " + name);
+        System.out.print("\n");
+
+        System.out.print("Masukan nama barang yang mau ditambah : ");
+        productName = input.nextLine();
+        System.out.print("\n");
+
+        System.out.print("Masukan jumlah barang yang mau ditambah : ");
+        productNumber = input.nextInt();
+        System.out.print("\n");
+
+        System.out.print("Masukan harga beli untuk barang tersebut : ");
+        buyPrice = input.nextDouble();
+        System.out.print("\n");
+
+        System.out.print("Masukan harga jual untuk barang tersebut : ");
+        sellPrice = input.nextDouble();
+        System.out.print("\n");
+
+        System.out.println("Stok Gudang Rincian Barang");
+        System.out.println("##########################\n\n");
+
+        System.out.println("Nama Barang     : " + productName);
+        System.out.println("Jumlah barang   : " + productNumber);
+        System.out.println("Harga Beli      : Rp." + String.format("%.2f", buyPrice));
+        System.out.println("Harga Jual      : RP." + String.format("%.2f", sellPrice));
     }
 
     public static void ramalan() {
@@ -62,47 +104,7 @@ class TP1 {
 
 
 
-    public static void gudang() {
-        Scanner input = new Scanner(System.in);
-
-        String name, productName;
-        double buyPrice, sellPrice;
-        int productNumber;
-
-        System.out.print("Masukan nama anda : ");
-        name = input.nextLine();
-
-        System.out.print("\n");
-        System.out.println("Stok Gudang Input Barang");
-        System.out.println("##########################\n\n");
-
-        System.out.println("Selamat Datang " + name);
-        System.out.print("\n");
-
-        System.out.print("Masukan nama barang yang mau ditambah : ");
-        productName = input.nextLine();
-        System.out.print("\n");
-
-        System.out.print("Masukan jumlah barang yang mau ditambah : ");
-        productNumber = input.nextInt();
-        System.out.print("\n");
-
-        System.out.print("Masukan harga beli untuk barang tersebut : ");
-        buyPrice = input.nextDouble();
-        System.out.print("\n");
-
-        System.out.print("Masukan harga jual untuk barang tersebut : ");
-        sellPrice = input.nextDouble();
-        System.out.print("\n");
-
-        System.out.println("Stok Gudang Rincian Barang");
-        System.out.println("##########################\n\n");
-
-        System.out.println("Nama Barang     : " + productName);
-        System.out.println("Jumlah barang   : " + productNumber);
-        System.out.println("Harga Beli      : Rp." + String.format("%.2f", buyPrice));
-        System.out.println("Harga Jual      : RP." + String.format("%.2f", sellPrice));
-    }
+    
 
     public static void testWrite() {
         System.out.println("Hello World");
